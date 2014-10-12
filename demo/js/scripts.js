67214109f4b48359a0de40e10ee9afd736283736
MarceloXP <marceloxp@gmail.com>
2014-10-12 17:02:39 -0300

    - Demo files.

--- demo/js/scripts.js
+++ demo/js/scripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	$('#my-slider-1').simpleSlider();
+
+	$('#my-slider-1').simpleSlider
+	(
+		{
+			'width': 800,
+			'height': 300,
+		}
+	);
+
+	$('#my-slider-2').simpleSlider
+	(
+		{
+			'width': 400,
+			'height': 300,
+		}
+	);
+
 });
